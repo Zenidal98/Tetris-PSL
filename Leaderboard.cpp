0b4806b4f184a650dbbb72e09147b9ec547342fe
Francesco Passarella <95143250+Zenidal98@users.noreply.github.com>
2024-09-03 14:14:28 +0200

    Update Leaderboard.cpp

--- Leaderboard.cpp
+++ Leaderboard.cpp
@@ -6,7 +6,7 @@
 void Leaderboard::loadScores() {
     std::ifstream infile("scores.txt");
     int score;
-    num_scores = 0; // Reset del conteggio
+    num_scores = 0;                       // Reset del conteggio
 
     while (infile >> score) {
         if (num_scores < max_scores) {
@@ -23,13 +23,13 @@ void Leaderboard::saveScore(int score) {
 
     if (num_scores < max_scores) {
         scores[num_scores++] = score;
-    } else if (score > scores[num_scores - 1]) { // Se il punteggio è maggiore del più basso nella classifica
-        scores[num_scores - 1] = score; // Sostituisci il più basso
+    } else if (score > scores[num_scores - 1]) {     // Se il punteggio è maggiore del più basso nella classifica
+        scores[num_scores - 1] = score;              // Sostituisci il più basso
     }
 
     bubbleSort();
 
-    // Sovrascrivi il file con i nuovi punteggi ordinati
+                                                      // Sovrascrivi il file con i nuovi punteggi ordinati
     std::ofstream outfile("scores.txt");
     for (int i = 0; i < num_scores; ++i) {
         outfile << scores[i] << std::endl;
@@ -46,19 +46,19 @@ void Leaderboard::bubbleSort() {
                 swapped = true;
             }
         }
-        if (!swapped) break; // Se non ci sono stati scambi, l'array è già ordinato
+        if (!swapped) break;                               // Se non ci sono stati scambi, l'array è già ordinato
     }
 }
 
 void Leaderboard::showLeaderboard() {
-    clear();  // Clear the screen for the leaderboard
+    clear();  
 
     int max_y, max_x;
     getmaxyx(stdscr, max_y, max_x);
 
     mvprintw(1, max_x / 2 - 6, "Classifica");
 
-    int start_y = max_y / 2 - (num_scores / 2); // Centro verticale della classifica
+    int start_y = max_y / 2 - (num_scores / 2);              // Centro verticale della classifica
     for (int i = 0; i < num_scores; ++i) {
         mvprintw(start_y + i, max_x / 2 - 10, "%d. %d", i + 1, scores[i]);
     }
@@ -68,11 +68,10 @@ void Leaderboard::showLeaderboard() {
     refresh();
 
     int ch;
-    while ((ch = getch()) != 'm') {
-        // Aspetta che l'utente prema 'm'
+    while ((ch = getch()) != 'm') {                       // Aspetta l'input
+        
     }
 
-    // Torna al menu principale
-    Menu menu;
+    Menu menu;                                            // Torna al menu
     menu.showMainMenu();
 }
